772b2906ddd25cba95f2f1d3a6941d9302e65b33
ammo <solidifyarmor@gmail.com>
2023-04-13 01:39:49 +0630

    color adjustments

--- src/scripts/darkmode.js
+++
@@ -1,7 +0,0 @@
-const html = document.documentElement
-const darkmodeCheckbox = document.getElementById('darkmode-btn')
-
-darkmodeCheckbox.addEventListener('change', ({ target: { checked } }) => {
-    if (checked) html.classList.remove('dark')
-    else html.classList.add('dark')
-})
