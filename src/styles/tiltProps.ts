2d180b2fccb6d3275d129e0c01db18b338b8fb70
ammo <solidifyarmor@gmail.com>
2023-04-18 23:44:24 +0630

    title styles

---
+++ src/styles/tiltProps.ts
@@ -0,0 +1,10 @@
+// https://micku7zu.github.io/vanilla-tilt.js/
+
+export const tiltProps = {
+  "data-tilt": true,
+  "data-tilt-glare": true,
+  "data-tilt-max-glare": "0.2",
+  "data-tilt-scale": "1.05",
+  "data-tilt-max": "10",
+  "data-tilt-speed": "500",
+};
