e86d538a25bd80f368536bc4ed1b14a253ef2365
ammo <solidifyarmor@gmail.com>
2023-04-20 22:19:05 +0630

    highlight & markdown parser

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      langs: ['javascript'],
+      wrap: true,
+    }
+  }
 });
