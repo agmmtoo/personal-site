bc80fe8489d73627d952fa165d303485024e5f1b
ammo <solidifyarmor@gmail.com>
2023-07-09 22:29:54 +0630

    tilt-card, hero-title

--- src/styles/tiltProps.ts
+++ src/styles/tiltProps.ts
@@ -4,8 +4,8 @@ export const tiltProps = {
   "data-tilt": true,
   "data-tilt-glare": true,
   "data-tilt-max-glare": "0.2",
-  "data-tilt-scale": "1.05",
-  "data-tilt-max": "8",
+  "data-tilt-scale": "1.02",
+  "data-tilt-max": "6",
   "data-tilt-speed": "1000",
   "gyroscope": false,
 };
