29eaf43ae005e99e828be57dc90fa1c8c2f3f9b9
ammo <solidifyarmor@gmail.com>
2023-07-09 21:33:51 +0630

    add "hidden" attribute in projects

--- src/types/Project.ts
+++ src/types/Project.ts
@@ -5,4 +5,5 @@ export default interface Project {
     url: string;
     slug: string;
     tags: string[];
+    hidden?: boolean;
   }
--- src/utils/getProjects.ts
+++ src/utils/getProjects.ts
@@ -3,7 +3,7 @@ import projects from "../data/projects.json";
 import Project from "../types/Project";
 
 export default function getProjects(): Project[] {
-  return projects;
+  return projects.filter((project) => !project.hidden);
 }
 
 export function getFeaturedProjects(): Project[] {
